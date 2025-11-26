ec7a3418c896450fc283fb75b7c57124325004d1
Kain0152 <dmeserakov623@gmail.com>
2025-11-26 16:29:58 +1000

    Fixed CORS

--- server.js
+++ server.js
@@ -18,14 +18,20 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
+// НАСТРОЙКИ CORS ДЛЯ RENDER
 app.use(cors({
   origin: [
     'https://site-food-accounting-frontend.onrender.com',
     'http://localhost:3000',
     'http://localhost:5173'
   ],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
+
+// Обработка preflight запросов
+app.options('*', cors());
 
 app.use(express.json());
 
